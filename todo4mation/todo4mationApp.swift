d084df9e835737e7ac7a141a9ed382e37d4be1f4
haronxbelghit <haronbel@gmail.com>
2023-04-06 19:40:22 +0000

    Initial Commit

---
+++ todo4mation/todo4mationApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo4mationApp.swift
+//  todo4mation
+//
+//  Created by Belghit Haron on 6/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct todo4mationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
