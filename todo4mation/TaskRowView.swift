0c162afeb0da0917f96dbed41e57c83f8d08a71f
haronxbelghit <haronbel@gmail.com>
2023-04-07 10:40:23 +0000

    added "marking a task as done" feature

--- todo4mation/TaskRowView.swift
+++ todo4mation/TaskRowView.swift
@@ -8,17 +8,42 @@
 import SwiftUI
 
 struct TaskRowView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    @FetchRequest(entity: Task.entity(),
+                  sortDescriptors: [NSSortDescriptor(keyPath: \Task.date, ascending: true)])
+    private var tasks: FetchedResults<Task>
+    
+    private func updateTask(_ task: Task) {
+        task.done = !task.done
+        do {
+            try viewContext.save()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
     let task: Task
     
     var body: some View {
         HStack {
             Text(task.title ?? "Untitled")
             Spacer()
+            Button(action: { updateTask(task) }) {
+                Image(systemName: task.done ? "checkmark.square.fill" : "square")
+                    .resizable()
+                    .frame(width: 22, height: 22)
+                    .foregroundColor(task.done ? .blue : .gray)
+            }
         }
     }
 }
+
+
+
 struct TaskRowView_Previews: PreviewProvider {
     static var previews: some View {
         TaskRowView(task: Task())
     }
 }
+
